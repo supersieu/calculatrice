e8af33f7f44c66605bdbe588dbb36ac9264616b6
nguyen ngoc dang <ngn1673a@u3-201-02l.ens.fsi>
2019-11-27 09:43:57 +0100

    Fonction conversion inverse

--- calculatrice.py
+++ calculatrice.py
@@ -33,3 +33,62 @@ def cal_n_input_sous(char):
         somme = somme + retenu
     somme = somme + cal_1_input(char[-1])
     return somme
+
+
+def operation (op,nombre_1,nombre_2):
+    resultat = 0
+    if op == "+":
+        resultat = plus(nombre_1, nombre_2)
+    elif op == "-":
+        resultat = moins(nombre_1, nombre_2)
+    elif op == "*":
+        resultat = mul(nombre_1, nombre_2)
+    elif op == "/":
+        resultat = div(nombre_1, nombre_2)
+    else:
+        print("Invalide operation")
+    return resultat
+
+
+def div(nombre_1, nombre_2):
+    resultat = cal_n_input_sous(nombre_1) / cal_n_input_sous(nombre_2)
+    return resultat
+
+
+def mul(nombre_1, nombre_2):
+    resultat = cal_n_input_sous(nombre_1) * cal_n_input_sous(nombre_2)
+    return resultat
+
+
+def moins(nombre_1, nombre_2):
+    resultat = cal_n_input_sous(nombre_1) - cal_n_input_sous(nombre_2)
+    return resultat
+
+
+def plus(nombre_1, nombre_2):
+    resultat = cal_n_input_sous(nombre_1) + cal_n_input_sous(nombre_2)
+    return resultat
+
+
+def convertir_1_c(nombre_decimal):
+    list_char = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
+    for i in range(len(list_char)):
+        if list_char[i][1] == nombre_decimal:
+            char = list_char[i][0]
+    return char
+
+
+def coef_chif(nombre_dec):
+    n=0
+    while nombre_dec != 0:
+        nombre_dec=nombre_dec//10
+        n=n+1
+    return n-1
+
+def
+
+
+def convertir(nombre_dec):
+    list=[["I", 1],["II", 2], ["III", 3], ["IV", 4], ["V", 5], ["VI", 6], ["VII",7], ["VIII", 8],["IX",9],["X",10]]
+    l=[]
+    while nombre_dec!=0:
--- tests/test_calculatrice.py
+++ tests/test_calculatrice.py
@@ -27,3 +27,47 @@ def test_cal_n_input():
 def test_cal_n_input_sous():
     assert calculatrice.cal_n_input_sous("IIV") == 5
     assert calculatrice.cal_n_input_sous("MCMXLIV") == 1944
+    assert calculatrice.cal_n_input_sous("DIX") == 509
+    assert calculatrice.cal_n_input_sous("XLV") == 45
+    assert calculatrice.cal_n_input_sous("VIII") == 8
+    assert calculatrice.cal_n_input_sous("MCMXCIX") == 1999
+
+
+def test_plus():
+    assert calculatrice.plus("IIV","IIV") == 10
+
+
+def test_moins():
+    assert calculatrice.moins("IIV","IIV") == 0
+
+
+def test_mul():
+    assert calculatrice.mul("IIV","IIV") == 25
+
+
+def test_div():
+    assert calculatrice.div("IIV","IIV") == 1
+
+
+def test_operation():
+    assert calculatrice.operation("+", "III", "MCMXLIV") == 1947
+    assert calculatrice.operation("-", "IIV","IIV") == 0
+    assert calculatrice.operation("*", "IIV","IIV") == 25
+    assert calculatrice.operation("/", "IIV","IIV") == 1
+
+
+def test_convertir_1_c():
+    assert calculatrice.convertir_1_c(1) == "I"
+    assert calculatrice.convertir_1_c(5) == "V"
+    assert calculatrice.convertir_1_c(10) == "X"
+    assert calculatrice.convertir_1_c(50) == "L"
+    assert calculatrice.convertir_1_c(100) == "C"
+    assert calculatrice.convertir_1_c(500) == "D"
+    assert calculatrice.convertir_1_c(1000) == "M"
+
+
+def test_coef_chif():
+    assert calculatrice.coef_chif(1900)==3
+    assert calculatrice.coef_chif(190) == 2
+    assert calculatrice.coef_chif(19) == 1
+    assert calculatrice.coef_chif(1) == 0
